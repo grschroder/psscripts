fdc4288e7de2f3ae2330d65df7a2fd4e867f2761
Leonardo Souza <leonardomv1@gmail.com>
2018-10-01 11:20:02 -0300

    Update Change-DriveLetter.ps1
    Alterando para as informações serem inseridas via variável.

--- windows-general/Change-DriveLetter.ps1
+++ windows-general/Change-DriveLetter.ps1
@@ -1,5 +1,13 @@
-#Author: https://github.com/grschroder
-#Description: This script will change the drive letter of a volume.
+#Author: Guilherme Schroder
+#Link: https://github.com/grschroder
+#Description: Script que altera a Letra correspondendo a um volume de disco e sua Label.
+#Last Modified: 01/10/2018
+#Last Modified by: Leonardo Souza - https://github.com/leonardomv1
 
-$drive = Get-WmiObject -Class win32_volume -Filter "DriveLetter = 'H:'"
-Set-WmiInstance -input $drive -Arguments @{DriveLetter="L:"; Label="Logs"}
+$ErrorActionPreference = "Stop"
+$driveold = READ-HOST "Enter the disk Letter 'A:'"
+$drivenew = READ-HOST "Enter the new dis Letter 'B:'"
+$label = READ-HOST "Enter the new disk Label"
+
+$drive = Get-WmiObject -Class win32_volume -Filter "DriveLetter = '$driveold'"
+Set-WmiInstance -input $drive -Arguments @{DriveLetter="$drivenew"; Label="$label"}
