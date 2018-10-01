31b1d5e3a9daac4785f662bceea2e85c7454629d
Leonardo Souza <leonardomv1@gmail.com>
2018-10-01 11:24:18 -0300

    Update Attach-ISiso.ps1
    Alterando para que as informações de endereço da ISO sejam adicionadas via variável.

--- hyperv-failovercluster/Attach-ISiso.ps1
+++ hyperv-failovercluster/Attach-ISiso.ps1
@@ -1,6 +1,10 @@
-#Author: https://github.com/grschroder
+#Author: Guilherme Schroder
+#Link: https://github.com/grschroder
 #Description: This script attach integration services iso in ALL of your VMs in a failover cluster. Execute from a cluster node.
+#Last Modified: 01/10/2018
+#Last Modified by: Leonardo Souza - https://github.com/leonardomv1
 
+$isoAddress = Read-Host "Enter the address for iso file. Example: C:\Windows\System32\vmguest.iso"
 
 $hvs = Get-ClusterNode
 foreach( $hv in $hvs ){
@@ -8,7 +12,7 @@ foreach( $hv in $hvs ){
         $Machines = Get-VM
         Foreach ($Machine in $Machines) {            
 	        write-host "Attaching iso..." $Machine.name
-            Set-VMDvdDrive $Machine.name -Path "C:\Windows\System32\vmguest.iso" -AllowUnverifiedPaths         
+            Set-VMDvdDrive $Machine.name -Path "$isoAddress" -AllowUnverifiedPaths         
         }
    }
 }
