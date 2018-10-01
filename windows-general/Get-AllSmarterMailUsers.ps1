0f93ba51ad965b386e1270954476a4ec4755ef89
Leonardo Souza <leonardomv1@gmail.com>
2018-10-01 11:16:38 -0300

    Update Get-AllSmarterMailUsers.ps1
    Alterando para as informações serem inseridas via variáveis.

--- windows-general/Get-AllSmarterMailUsers.ps1
+++ windows-general/Get-AllSmarterMailUsers.ps1
@@ -1,11 +1,17 @@
-#Author: https://github.com/grschroder
+#Author: Guilherme Schroder
+#Link: https://github.com/grschroder
 #Description: This script will export all smartermail users to a file.
+#Last Modified: 01/10/2018
+#Last Modified by: Leonardo Souza - https://github.com/leonardomv1
 
-$domains = Get-Content -Path "C:\Program Files (x86)\SmarterTools\SmarterMail\Service\domainList.xml"
+$domainListAddress = READ-HOST "Enter the complete address where the domainList file is stored. Example: C:\Program Files (x86)\SmarterTools\SmarterMail\Service\domainList.xml"
+$accountListAddress = READ-HOST "Enter the address where the domains is stored. Example: C:\Smartermail\Domains\"
+$result = READ-HOST "Enter the complete address where the result file will be stored"
+$domains = Get-Content -Path "$domainsListAdress"
 foreach ($domain in $domains){    
-        [xml]$xmlusers = Get-Content -Path "C:\Smartermail\Domains\$domain\accountList.xml"
+        [xml]$xmlusers = Get-Content -Path "$accountListAddress+$domain\accountList.xml"
         $userst = $xmlusers.AddressList.Address.name        
         foreach ($user in $userst){            
-            $user+"@"+$domain >> "C:\smUsers.txt"
+            $user+"@"+$domain >> "$result"
         }
 }
